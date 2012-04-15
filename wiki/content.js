d49e0dea8359cf2acfe9391887ae9c9172c45338
Dorsey Hu <hzqlmm@gmail.com>
2012-04-15 21:46:55 +0800

    add sidebar menu

--- wiki/content.js
+++ wiki/content.js
@@ -31,7 +31,7 @@ var body;
 $(document).ready(function () {
     originalPage = $('html').html();
 
-    var menu = $("<div id='mcs_container'><div class='customScrollBox'> <div class='horWrapper'> <div class='container'> <div class='content'></div> </div> <div class='dragger_container'> <div class='dragger'></div> </div> </div> </div> </div>");
+    var menu = $("<div class='sidebar_menu'><p>MENU</p><p>TRACK</p><p>BACK</p></div><div class='menucontainer'><div class='close_icon'><p>+</p></div><div id='mcs_container' class='translate'><div class='customScrollBox'> <div class='horWrapper'> <div class='container'> <div class='content'></div> </div> <div class='dragger_container'> <div class='dragger'></div> </div> </div> </div> </div></div>");
 
     $('#catlinks,span.tocnumber,script,div.printfooter,#mw-articlefeedback,div.articleFeedback-panel div.references-column-width,hr,head,#protected-icon,a#top,#mw-js-message,#siteNotice,#jump-to-nav,#contentSub,#mw-head,#mw-panel,#footer,#mw-page-base,#mw-head-base,#siteSub,sup,table.ambox-style').remove();
     $('#References').parent().find('~*').remove();
@@ -48,6 +48,7 @@ $(document).ready(function () {
         $('body').prepend(menu);
         $('div.dragger').css('height', '60px');
         $('a[href=#References]').parent().find('~*').remove();
+        $('a[href=#References]').parent().remove();
     }
 
     var left = $('div#bodyContent').offset().left;
@@ -60,19 +61,51 @@ $(document).ready(function () {
     $('#bodyContent').css('color', 'white');
     $('#content').offset({top:0});
 
-    $('#content').hover(
+    $('#content,#mcs_container').hover(
         function () {
-            $('a').css('color', 'lightblue');
+            $(this).find('a').css('color', 'lightblue');
         },
         function () {
-            $('a').css('color', 'white');
-        }
-    );
+            $(this).find('a').css('color', 'white');
+        });
 
     $('body').css('backgroundImage', 'url(' + chrome.extension.getURL("background.jpg") + ')');
-    $('#content').css('height', $('#content').height() + 20);
+    $('#content').css({'height':$('#content').height() + 20, 'top':0});
     $("#mcs_container").mCustomScrollbar("vertical", 1000, "easeOutCirc", 1.05, "fixed", "yes", "yes", 20);
+    $('div.menucontainer').css('left', '-20%');
+    $(".close_icon").hide();
 
+    $(".close_icon").click(function () {
+        $('div.menucontainer').animate({left:'-20%'}, 1000, function () {
+            $('.sidebar_menu').animate({left:0}, 500);
+        })
+    });
+
+    $('div.menucontainer').hover(
+        function () {
+            var left = $('div.dragger_container').offset().left;
+            $(".close_icon").css('left', left - 14);
+            $(".close_icon").show();
+        },
+        function () {
+            $('.close_icon').hide();
+        });
+
+    $('#mcs_container').hover(
+        function () {
+            var left = $('div.dragger_container').offset().left;
+            $(".close_icon").css('left', left - 14);
+            $(".close_icon").show();
+        });
+
+    $('.sidebar_menu >p').click(function () {
+        if ($(this).text() == 'MENU') {
+            $('.sidebar_menu').animate({left:'-20%'}, 500, function () {
+                $('div.menucontainer').animate({left:0}, 500);
+            })
+        }
+    });
+    window.scrollTo(0, 0);
 });
 
 function isScrolledIntoView(elem) {
